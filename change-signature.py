75fe68a977daedca10813d65cda9e98673e9bb17
Chris Cartland <c.cartland@gmail.com>
2018-09-26 22:31:28 -0700

    Update LICENSE and copyright

--- change-signature.py
+++ change-signature.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #
-# Copyright 2018 Chris Cartland
+# Copyright 2018 Chris Cartland. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
